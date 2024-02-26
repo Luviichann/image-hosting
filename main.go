ee2466cc8343bb8c37b5228515236c1963025d3e
Luviichann <anddiaoling777@163.com>
2024-02-26 20:21:23 +0800

    main program

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"goimg/routers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/img", "./img")
+	routers.RoutersInit(r)
+	r.Run()
+}
+
+// github.com/pilu/fresh
